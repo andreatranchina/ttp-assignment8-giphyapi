6802f152eb8e0bd2abcdeab5bcbddea551204a42
Andrea <Andrea.Tranchina82@gmail.com>
2023-06-19 15:41:54 -0400

    setup of app component to display trending gifs on page load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [giphyList, setGiphyList] = useState([]);
+
+
+  useEffect(() => {
+    async function fetchGiphys(){
+      try{
+        const list = await axios.get("https://api.giphy.com/v1/gifs/trending?api_key=QUKQWldQ4ev84lpETKvbdYU6MVUaNx7f");
+        let fetchGiphys = [...list.data.data];
+        setGiphyList(fetchGiphys);
+        console.log(fetchGiphys);
+      }
+      catch(error){
+        console.log(error);
+      }
+    }
+    fetchGiphys();
+  }, []);
   return (
     <div className="App">
-
+      {giphyList.map((giphy)=>{
+        return (<img src={giphy.images.fixed_height.url} />)
+      })}
     </div>
   );
 }
